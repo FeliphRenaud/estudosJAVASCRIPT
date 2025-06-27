73e7f248167651419362dfac7483a1713e8e4888
feliph <feliphrenaud@gmail.com>
2025-06-27 16:10:00 -0300

    commit de inicialização da branch e navegação pelo DOM

---
+++ DOM/src/js/index.js
@@ -0,0 +1,8 @@
+// movendo-se pelo dom
+
+console.log(document.body);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1].childNodes);
+console.log(document.body.childNodes[1].childNodes[1].textContent);
+
+
